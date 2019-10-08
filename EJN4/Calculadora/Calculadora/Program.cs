2c11aafc6b55cf8f30170a6b18cd1f11b5c1f8ab
cristian grimaldi <grimaldi.cristian@gmail.com>
2019-10-07 21:49:50 -0300

    Calculadora

---
+++ EJN4/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Calculadora
+    {
+        float Resultado = 0;
+
+        Calculadora()
+        {
+            Resultado = 0;
+        }
+
+        public Double Calcular(float valorA, float valorB, string operacion)
+        {
+            switch (operacion)
+            {
+                case "+":
+                    suma(valorA, valorB);
+                    break;
+                case "-":
+                    resta(valorA, valorB);
+                    break;
+                case "*":
+                    multiplicacion(valorA, valorB);
+                    break;
+                case "/":
+                    division(valorA, valorB);
+                    break;
+            }
+
+            return Resultado;
+        }
+
+        private void division(float valorA, float valorB)
+        {
+            try
+            {
+                Resultado = valorA / valorB;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("No se puede dividir por cero");
+            }
+
+        }
+
+        private void multiplicacion(float valorA, float valorB)
+        {
+            Resultado = valorA * valorB;
+        }
+
+        private void resta(float valorA, float valorB)
+        {
+            Resultado = valorA - valorB;
+        }
+
+        private void suma(float valorA, float valorB)
+        {
+            Resultado = valorA + valorB;
+        }
+
+        public float GetResultado()
+        {
+            return Resultado;
+        }
+    }
+}
