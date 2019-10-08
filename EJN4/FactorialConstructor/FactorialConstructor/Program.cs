2479c848a7c8ddd6e1053ed77cd1841f4a32f8a5
cristian grimaldi <grimaldi.cristian@gmail.com>
2019-10-07 21:57:53 -0300

    Factorial

---
+++ EJN4/FactorialConstructor/FactorialConstructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FactorialConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial factorial = new Factorial(5);
+            Console.WriteLine(factorial.GetResultado());
+        }
+    }
+
+    class Factorial
+    {
+        double Resultado;
+        public Factorial(double nro)
+        {
+            if (nro == 1)
+                Resultado = nro;
+            else
+                Resultado = nro * new Factorial(nro - 1).Resultado;
+        }
+
+        public double GetResultado() {
+            return Resultado;
+        }
+    }
+}
